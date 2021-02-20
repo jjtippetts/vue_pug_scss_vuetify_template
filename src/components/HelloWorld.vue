5dc0b509e33a7977f46c4ab8e391cfd12960510e
Jordan <45724546+jjtippetts@users.noreply.github.com>
2021-02-19 23:17:01 -0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+div
+  h1 Component Title
+  p {{msg}}
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+
+</style>
